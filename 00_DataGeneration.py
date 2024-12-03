150a025edfef0ead98e9a10691a75973d0cdb71d
gabri-al <gabri-al>
2024-12-03 12:15:19 +0000

    Data gen completed

--- 00_DataGeneration.py
+++ 00_DataGeneration.py
@@ -14,6 +14,7 @@ from faker import Faker
 import pandas as pd
 from collections import OrderedDict 
 from datetime import datetime, timedelta
+import random
 
 from pyspark.sql import functions as F
 
@@ -24,21 +25,42 @@ from pyspark.sql import functions as F
 
 # COMMAND ----------
 
+_catalog = 'users'
+_schema = 'gabriele_albini'
+_volume = 'DE_demo_files'
+
+spark.sql("CREATE CATALOG IF NOT EXISTS "+_catalog)
+spark.sql("CREATE SCHEMA IF NOT EXISTS "+_catalog+"."+_schema)
+spark.sql("CREATE VOLUME IF NOT EXISTS "+_catalog+"."+_schema+"."+_volume)
+spark.sql("USE CATALOG "+_catalog)
+spark.sql("USE SCHEMA "+_schema)
+
+# COMMAND ----------
+
 # Initialize Faker
 fake = Faker()
+SEED_START = 500
 
 # Dataset Size
-Ncustomers = 100000
-Norders = 3 * Ncustomers
+Ncustomers = 75000
+Norders = round(1.5 * Ncustomers)
+Nmarketing = round(2.5 * Ncustomers)
+
+# Dates
+start = datetime.now() - timedelta(days=30*30)
+end = datetime.now() - timedelta(days=4*30)
+
+# Path to volume to write csv
+path_volume_ = f"/Volumes/{_catalog}/{_schema}/{_volume}/"
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ### Generate Customers
+# MAGIC ### Generate Customers Data
 
 # COMMAND ----------
 
-# Generate customers
+# Generate customers function
 def gen_customers(N):
 
   # Variables
@@ -46,16 +68,16 @@ def gen_customers(N):
   countries_ = OrderedDict([("Italy", 0.05),("USA", 0.4),("Brasil", 0.4),("Portugal", 0.15)])
 
   # Initialize empty lists
-  id_ = []; first_name = []; last_name = []; email = []; address = []; country = []; phone = []; channel = []
+  id_ = []; first_name = []; last_name = []; email = []; address = []; country = []; phone = []; channel = []; dates = []
   
   # Generate data
   for i in range(N):
 
     # Define random seed
-    fake.seed_instance(1000+i)
+    fake.seed_instance(SEED_START+i)
 
     # Generate data
-    id_.append(fake.uuid4())
+    id_.append('c-'+fake.uuid4())
     first_name.append(fake.first_name())
     last_name.append(fake.last_name())
     email.append(fake.ascii_company_email())
@@ -63,11 +85,12 @@ def gen_customers(N):
     country.append(fake.random_elements(elements = countries_, length = 1)[0])
     phone.append(fake.phone_number())
     channel.append(fake.random_elements(elements = channels_, length = 1)[0])
+    dates.append(fake.date_time_between(start_date=start, end_date=end).strftime("%Y-%m-%d %H:%M:%S"))
 
   return pd.DataFrame({
-    'id': id_, 'firstname': first_name, 'lastname': last_name,
+    'clientid': id_, 'firstname': first_name, 'lastname': last_name,
     'email': email, 'address': address, 'country': country,
-    'phone': phone, 'channel': channel})
+    'phone': phone, 'channel': channel, 'creation_date': dates})
 
 # COMMAND ----------
 
@@ -76,4 +99,99 @@ display(df_customers.head(7))
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ### Generate Orders Data
 
+# COMMAND ----------
+
+def gen_orders(N):
+
+  # Initialize empty lists
+  id_ = []; dates = []; items = []; amount = []
+  
+  # Generate data
+  customerid = random.choices(list(df_customers['clientid']), k=N)
+  for i in range(N):
+
+    # Define random seed
+    fake.seed_instance(2*SEED_START + i)
+
+    # Generate data
+    id_.append('o-'+fake.uuid4())
+    dates.append(fake.date_time_between(start_date=start, end_date=end).strftime("%Y-%m-%d %H:%M:%S"))
+    item = round(fake.random.uniform(1, 4))
+    items.append(item)
+    amount.append(round( item * (fake.random.uniform(0,1) + 4)  ) )
+
+  return pd.DataFrame({
+    'orderid': id_, 'transactiondate': dates,
+    'items': items, 'amount': amount, 'customerid': customerid})
+
+# COMMAND ----------
+
+df_orders = gen_orders(Norders)
+display(df_orders.head(7))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Marketing Activity
+
+# COMMAND ----------
+
+# Generate marketing data function
+def gen_marketing(N):
+
+  # Variables
+  marketing_activities_ = OrderedDict([("SMS", 0.33),("email", 0.33),("bunner", 0.33)])
+
+  # Initialize empty lists
+  id_ = []; dates = []; activity = []; clicked = []
+  
+  # Generate data
+  customerid = random.choices(list(df_customers['clientid']), k=N)
+  for i in range(N):
+
+    # Define random seed
+    fake.seed_instance(3*SEED_START + i)
+
+    # Generate data
+    id_.append('a-'+fake.uuid4())
+    dates.append(fake.date_time_between(start_date=start, end_date=end).strftime("%Y-%m-%d %H:%M:%S"))
+    clicked.append(round(fake.random.uniform(0, 1)))
+
+  return pd.DataFrame({
+    'marketingid': id_, 'marketingdate': dates,
+    'clicked': clicked, 'targetedcustomerid': customerid})
+
+# COMMAND ----------
+
+df_marketing = gen_marketing(Nmarketing)
+display(df_marketing.head(7))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Write to CSV
+
+# COMMAND ----------
+
+size1 = round(Ncustomers * 0.75 + 3)
+
+# Customers
+df_customers1 = df_customers.iloc[:size1, :]
+df_customers1.to_csv(path_volume_+"/customers_001.csv", index=False, mode='w')
+df_customers2 = df_customers.iloc[size1+22:, :]
+df_customers2.to_csv(path_volume_+"/customers_002.csv", index=False, mode='w')
+
+# Orders
+df_orders1 = df_orders.loc[df_orders['customerid'].isin(df_customers1['clientid']), :]
+df_orders1.to_csv(path_volume_+"/orders_001.csv", index=False, mode='w')
+df_orders2 = df_orders.loc[~df_orders['customerid'].isin(df_customers1['clientid']), :]
+df_orders2.to_csv(path_volume_+"/orders_002.csv", index=False, mode='w')
+
+# Marketing Activity
+df_marketing1 = df_marketing.loc[df_marketing['targetedcustomerid'].isin(df_customers1['clientid']), :]
+df_marketing1.to_csv(path_volume_+"/marketing_001.csv", index=False, mode='w')
+df_marketing2 = df_marketing.loc[~df_marketing['targetedcustomerid'].isin(df_customers1['clientid']), :]
+df_marketing2.to_csv(path_volume_+"/marketing_002.csv", index=False, mode='w')
